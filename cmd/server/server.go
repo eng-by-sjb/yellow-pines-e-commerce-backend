5ca85e852a32c53325001f9d6a45ff6043d6d507
Stephen Jersuit Benyah <stephen.benyah.dev@gmail.com>
2025-01-20 16:38:50 +0000

    refactor: add a v1Router method on server.
    - add method on server to hold version one of the api for better structuring.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -28,7 +28,6 @@ func NewServer(addr string, db *sql.DB, tokenManager *auth.TokenService) *Server
 
 func (s *Server) Start() error {
 	router := chi.NewRouter()
-	v1Router := chi.NewRouter() // api version 1 subrouter
 
 	// strip trailing slashes at the end of the url
 	// e.g. /users/1/ -> /users/1
@@ -36,21 +35,7 @@ func (s *Server) Start() error {
 	// to ensure that the url is correctly formatted
 	router.Use(chimiddleware.StripSlashes)
 
-	// health check
-	v1Router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
-
-	// user feature
-	userStore := user.NewStore(s.db)
-	userService := user.NewService(userStore, s.tokenManager)
-	userHandler := user.NewHandler(userService)
-	userHandler.RegisterRoutes(v1Router)
-
-	// products feature
-
-	router.Mount("/api/v1", v1Router)
+	router.Mount("/api/v1", s.v1Router()) // api version 1 subrouter
 
 	srv := http.Server{
 		Addr:    fmt.Sprintf(":%s", s.addr),
@@ -62,11 +47,20 @@ func (s *Server) Start() error {
 	return srv.ListenAndServe()
 }
 
-// func (s *Server) userFeature(router *chi.Mux)  {
-// 	// user feature
-// 	userStore := user.NewStore(s.db)
-// 	tokenMaker := auth.NewTokenMaker()
-// 	userService := user.NewService(userStore, tokenMaker)
-// 	userHandler := user.NewHandler(userService)
-// 	userHandler.RegisterRoutes(router)
-// }
+func (s *Server) v1Router() *chi.Mux {
+	r := chi.NewRouter()
+
+	// health check
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	// user feature
+	userStore := user.NewStore(s.db)
+	userService := user.NewService(userStore, s.tokenManager)
+	userHandler := user.NewHandler(userService)
+	userHandler.RegisterRoutes(r)
+
+	return r
+}
